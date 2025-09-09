1e8003733bf21f730a70310538f34295b22a8ec4
Mithilesh Lohar <128236295+mithlohar144@users.noreply.github.com>
2025-09-09 22:00:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(helmet());
 
 // CORS configuration
 const allowedOrigins = [
-  'https://astrodrkanhaiya.in/',
+  'https://astrodrkanhaiya.in',
   'http://127.0.0.1:3000',
   'http://localhost:8000',
   'http://127.0.0.1:8080',
